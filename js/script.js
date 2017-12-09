e36eadb90082d79759a99b4b1bdfa8225c5ef197
Yurii Pavlenko <mark.orefa@gmail.com>
2017-12-09 11:13:45 +0200

    #52 Завершаем вёрстку личного проекта JavaScript for index.html

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var bookmark = document.querySelectorAll('.bookmark');
+var headerBookmark = document.querySelector('.header-bookmark');
+var writeUs = document.querySelector('.contacts-feedback');
+var map = document.querySelector('contacts-map');
+var modalContainer = document.querySelector('.modal-container');
+var feedbackPopup = modalContainer.querySelector('.modal-feedback');
+var closeFeedback = feedbackPopup.querySelector(".close-popup");
+var mapPopup = modalContainer.querySelector('.map-popup');
+var closeMapPopup = mapPopup.querySelector('close-popup');
+
+writeUs.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.add('modal-show');
+});
+
+closeFeedback.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove('modal-show');
+});
+
+map.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add('modal-show');
+});
+
+closeMapPopup.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove('modal-show');
+});
