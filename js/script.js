807fb4570ff1e2fee570bb9968c86d6bf4ea666c
Yurii Pavlenko <mark.orefa@gmail.com>
2017-12-09 15:37:17 +0200

    #52 Завершаем вёрстку личного проекта JavaScript mini added

--- js/script.js
+++ js/script.js
@@ -1,32 +1,42 @@
-var writeUs = document.querySelector('.contacts-feedback');
-var map = document.querySelector('.contacts-map');
-var modalContainer = document.querySelector('.modal-container');
-var feedbackPopup = modalContainer.querySelector('.modal-feedback');
-var closeFeedback = feedbackPopup.querySelector(".close-popup");
-var mapPopup = modalContainer.querySelector('.map-popup');
-var closeMapPopup = mapPopup.querySelector('.close-popup');
 var buyButton = document.querySelectorAll('.buy');
-var cartPopup = document.querySelector('.modal-cart');
+var modalContainer = document.querySelector('.modal-container');
+var cartPopup = modalContainer.querySelector('.modal-cart');
+var closeCartPopup = cartPopup.querySelector('.close-popup');
+var writeUs;
+var map;
+var feedbackPopup;
+var closeFeedback;
+var mapPopup;
+var closeMapPopup;
 
-writeUs.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  feedbackPopup.classList.add('modal-show');
-});
+if (document.querySelector('.main-container')) {
+  writeUs = document.querySelector('.contacts-feedback');
+  map = document.querySelector('.contacts-map');
+  feedbackPopup = modalContainer.querySelector('.modal-feedback');
+  closeFeedback = feedbackPopup.querySelector(".close-popup");
+  mapPopup = modalContainer.querySelector('.map-popup');
+  closeMapPopup = mapPopup.querySelector('.close-popup');
 
-closeFeedback.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  feedbackPopup.classList.remove('modal-show');
-});
+  writeUs.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.add('modal-show');
+  });
 
-map.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.add('modal-show');
-});
+  closeFeedback.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove('modal-show');
+  });
 
-closeMapPopup.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.remove('modal-show');
-});
+  map.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add('modal-show');
+  });
+
+  closeMapPopup.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove('modal-show');
+  });
+}
 
 for (var i = 0; i < buyButton.length; i++) {
   buyButton[i].addEventListener('click', function (evt) {
@@ -34,3 +44,8 @@ for (var i = 0; i < buyButton.length; i++) {
     cartPopup.classList.add('modal-show');
   })
 }
+
+closeCartPopup.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove('modal-show');
+});
