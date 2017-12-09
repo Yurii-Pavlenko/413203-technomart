f620d1eff435ef7ff109e3b81dda6ce5baa32b96
Yurii Pavlenko <mark.orefa@gmail.com>
2017-12-09 11:54:44 +0200

    #52 Завершаем вёрстку личного проекта JavaScript fixed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 var bookmark = document.querySelectorAll('.bookmark');
 var headerBookmark = document.querySelector('.header-bookmark');
 var writeUs = document.querySelector('.contacts-feedback');
-var map = document.querySelector('contacts-map');
+var map = document.querySelector('.contacts-map');
 var modalContainer = document.querySelector('.modal-container');
 var feedbackPopup = modalContainer.querySelector('.modal-feedback');
 var closeFeedback = feedbackPopup.querySelector(".close-popup");
 var mapPopup = modalContainer.querySelector('.map-popup');
-var closeMapPopup = mapPopup.querySelector('close-popup');
+var closeMapPopup = mapPopup.querySelector('.close-popup');
+var buyButton = document.querySelectorAll('.buy');
+var cartPopup = document.querySelector('.modal-cart');
 
 writeUs.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -27,3 +29,10 @@ closeMapPopup.addEventListener('click', function (evt) {
   evt.preventDefault();
   mapPopup.classList.remove('modal-show');
 });
+
+for (var i = 0; i < buyButton.length; i++) {
+  buyButton[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add('modal-show');
+  })
+}
